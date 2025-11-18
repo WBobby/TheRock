05dcb0458b50b24020da8eddc13136c84bcf4e7c
Jenkins <jenkins-compute@amd.com>
2025-11-18 17:32:12 +0000

    Address @stellaraccident's review:
    - Move dctools/RDC checks to subdirectory with FATAL_ERROR
    - Replace THEROCK_BUNDLED_GRPC with direct BUILD_DEPS
    - Refactor gRPC platform flags into pre_hook/post_hook
    - Add libcap with add_custom_target pattern
    - libcap symbol versioning (TODO: Add symbol versioning patches)
    
    Signed-off-by: Jenkins <jenkins-compute@amd.com>

---
+++ third-party/sysdeps/linux/libcap/patch_source.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+SOURCE_DIR="${1:?Source directory must be given}"
+
+# TODO: Add symbol versioning patches for libcap
+
+echo "libcap patch_source: Symbol versioning not yet implemented (TODO)"
+
